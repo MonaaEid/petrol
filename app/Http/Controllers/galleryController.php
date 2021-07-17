819680c02f1716d146d0b6d5530f6cf7de627243
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2021-07-17 17:27:27 +0200

    edit galleryController

--- app/Http/Controllers/galleryController.php
+++ app/Http/Controllers/galleryController.php
@@ -51,69 +51,60 @@ class galleryController extends Controller
     
     }
 
-    // public function galleryOrder(Request $request){
-    //     $gallery = Gallery::all(['id','product_name','stock_no']);
-    //     return view('makeGalleryOrder', compact('gallery'));
-    // }
 
-
+//function to make new order
     
     public function galleryOrder(Request $request){
         $gallery = Gallery::all(['id','product_name','stock_no']);
         return view('gallery.galleryOrderTwo', compact('gallery'));
     }
 
-    public function storeOrder(Request $request){
-        $request->validate( [
-            // 'date' => 'required',
-            'product.*' => 'required',
-            'quantity.*' => 'required|numeric',
-         
-        ]);
+    
+//fuction to save order
+public function storeOrder(Request $request){
+    $request->validate( [
+        'quantity' => 'required',
+        'product' => 'required',
+        'date'=>'required'
+       
+    ]);
+    $units = [];
+    $message= array();
 
-        /*DB::beginTransaction();
+    foreach ($request->product as $key => $product) {
+        $units[] = [
+           
+            'user_id' => 1,
+            'gallery_id' => $request->product[$key],
+            'amount' => $request->quantity[$key],
+            'date' => $request->date,
+        ];
+        $stock = \App\Gallery::where('id', $request->product[$key])->firstOrFail();
+        if($stock->stock_no-$request->quantity[$key]<0)
+        $message[] = 'Not enough quantity of product ' . $stock->product_name;
 
-        try {*/
-            for ($i=0;$i<count($request['product']);$i++)
-        {
-                $gallery_user = new Gallery_User();
-                $gallery_user->user_id = 1;
-                $gallery_user->date = date('2021-06-27');
-                $gallery_user->gallery_id = (int)$request['product'][$i];
-                $gallery_user->amount = (int)$request['quantity'][$i];
-                $stock = DB::table('gallery')
-                        ->where('id','=', $request['product'])
-                        ->pluck('stock_no');
-                        //dd($stock[0]<$request['quantity'][0]);
-                        dd($stock[1]);
-                        if($stock[$i]<$request['quantity'][$i]){
-                            $request->session()->flash('status', 'no enough qty!');
-                            //dd($request['quantity']);
-                            return redirect()->back();
-                            //dd('aya');
-                        }
-                $gallery_user->save();
-                        //dd('hhhhhh');
-                $product=Gallery::findorfail((int)$request['product'][$i]);
-                //dd($product);
-                $product->stock_no=$product->stock_no - $request['quantity'][$i];
-                $product->save();
 
-                $request->session()->flash('status', 'successful');
-                return redirect()->back();
-         }
+ 
+    } 
 
-           /* DB::commit();
-            // all good
-        } catch (\Exception $e) {
-            DB::rollback();
-            // something went wrong
-        }*/
+ 
 
-        // $gallery_id = Gallery::get('id'); 
-        // $gallery_qty= Gallery::get('stock_no'); 
-        
+    if(! empty($message)) {
+            return redirect()->back()->withErrors($message);
     }
+       
+  elseif  (! empty($units)) {
+
+            DB::table('gallery_user')->insert($units);
+            foreach ($request->product as $key => $product) {
+            DB::table('gallery')
+            ->where('id',$request->product[$key])->decrement('stock_no',$request->quantity[$key]);
+            }
+            $request->session()->flash('status', 'successful');
+            return redirect()->back();
+        }
+    }
+//funcction to fetch price with ajax
     public  function priceTwo(Request $request )
     {
         $query= $request->get('productID');
@@ -127,48 +118,20 @@ class galleryController extends Controller
         // ($op);
         return response()->json($op);
     }
+//funcction to fetch total price with ajax
+    public function totalPriceUpdate(Request $request){
+        $query1= $request->get('quantity');
+        $query2= $request->get('productID');
 
-    // public function totalPriceUpdate(Request $request){
-    //     $query1= $request->get('quantity');
-    //     $query2= $request->get('dishID');
-
-    //     $op = DB::table('dishes')
-    //         ->where('id','=', $query2)
-    //         ->pluck('dish_price');
-    //     $op=$op[0];
-    //     $price=$query1*$op;
-    //     return response()->json($price);
-    // }
-
-    
-
-// public function storeOrder(Request $request){
-      
-//     $units = [];
-//     $message= array();//place outside the loop
-//     $stock = [];
-//     // if($request->quantity<0)
-//     // dd($stock);
-   
-//     foreach ($request->product as $key => $product) {
-//         $units[] = [
-           
-//             'user_id' => 1,
-//             'gallery_id' => $request->product[$key],
-//             'amount' => $request->quantity[$key],
-//             'date' => date('2021-06-27'),
-//         ];
-//         $stock[]= DB::table('gallery')
-//             ->where('id',$request->product[$key])
-//             // ->where('stock_no','-',$request->quantity[$key],'>=',0)
-//             ->pluck('stock_no');
-//     } 
-//     dd($stock);
-//     // while($product->quantity - $item->qty<0);
-//     // $message[] = 'Not enough Product' . $item->id;
-// //  dd($request->product);
-//     if (! empty($units)) {
-//         DB::table('gallery_user')->insert($units);
-//     }
-//     }
+        $one = DB::table('gallery_pricing')
+            ->where('id', $query2)
+            ->pluck('pricing_id');
+        $op=DB::table('pricing')
+            ->where('id',$one)
+            ->pluck('sale_price');
+        $op=$op[0];
+        $price=$query1*$op;
+        return response()->json($price);
+    }
 }
+
