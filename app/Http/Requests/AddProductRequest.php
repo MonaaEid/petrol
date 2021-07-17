5b336cd83056bf9a7e3f423232869838de0fa9d9
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2021-07-17 17:17:26 +0200

    Add files via upload

---
+++ app/Http/Requests/AddProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // 'date' => 'required',
+           
+            'product_name' => 'required|min:2|max:15|unique:gallery',
+            'stock_no' => 'required|numeric'
+        ];
+    }
+}
