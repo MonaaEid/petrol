7df3afe0785665dfc2e779a06efe6414c88062a8
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2021-07-17 17:15:18 +0200

    adding models

---
+++ app/Gallery.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    protected $table = 'gallery';
+    
+    // public function gallery_user(){
+    //     return $this->belongsToMany('App\Gallery_User');
+    // }
+    //
+}
---
+++ app/Gallery_User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery_User extends Model
+{
+    protected $table = 'gallery_user';
+    //
+    // public function gallery(){
+    //     return $this->hasMany('App\Gallery');
+    // }
+}
