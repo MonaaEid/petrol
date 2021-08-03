7cf09648a2924a608441230d1bbb9ecdfb6c16a8
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2021-08-03 16:33:14 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,19 @@ Route::post('/editer/{id}/update','galleryController@update');
 
 Route::get('/priceTwo', 'galleryController@priceTwo')->name('priceTwo');
 Route::get('/totalPriceUpdate', 'galleryController@totalPriceUpdate')->name('totalPriceUpdate');
+
+//gallery pricing controller 
+Route::get('gallery-pricing','pricingController@galleryPricing');
+Route::post('store-gallery_pricing','pricingController@storeGalleryPricing');
+Route::get('gallery-pricing-list','pricingController@indexGalleryPricing');
+Route::get('/gallery-pricing-deleter/{id}','pricingController@deleteGalleryPricing');
+Route::get('/gallery-pricing-editer/{id}','pricingController@editGalleryPricing');
+Route::post('/gallery-pricing-editer/{id}/update','pricingController@updateGalleryPricing');
+
+//gallery pricing controller 
+Route::get('fuel-pricing','pricingController@fuelPricing');
+Route::post('store-fuel-pricing','pricingController@storeFuelPricing');
+Route::get('fuel-pricing-list','pricingController@indexFuelPricing');
+Route::get('/fuel-pricing-deleter/{id}','pricingController@deleteFuelPricing');
+Route::get('/fuel-pricing-editer/{id}','pricingController@editFuelPricing');
+Route::post('/fuel-pricing-editer/{id}/update','pricingController@updateFuelPricing');
